0e0b33ba6c2af4cca0c50efb45629c6fa8f1b8e3
Maciej Kaznowski <mmkaznowski@gmail.com>
2018-10-27 18:09:16 +0100

    Reformatted the code

--- UCL_project.py
+++ UCL_project.py
@@ -28,27 +28,28 @@ def main_menu():
     return selected_option
 
 
-def about():    # This function displays information about the programme.
+def about():  # This function displays information about the programme.
     print('All of the information is here!!!')
     any_key = input('Press any key to return to the main menu: ')
     if type(any_key) == str:
         pass
 
 
-def create_new_project():   # This function will return a project object, which will be added to projects list.
+def create_new_project():  # This function will return a project object, which will be added to projects list.
     project_name = str(input("Enter the project name: "))
     num_of_members = input("Enter the number of team members: ")
     while not (num_of_members.isdigit() and int(num_of_members) > 2):
         num_of_members = input("Incorrect input. Please enter the number of team members: ")
     num_of_members = int(num_of_members)
-    team_members = []   # Creates a list of the project members
+    team_members = []  # Creates a list of the project members
     print('\n')
     for i in range(num_of_members):
         member_name = str(input("     Enter the name of team member {}: ".format(i + 1)))
         # Checking if the same member is not entered twice
         while member_name in [member.name for member in team_members]:
-            member_name = str(input("     {} already in the team. Enter the name of team member {}: ".format(member_name, i + 1)))
-        team_members.append(Person(member_name))    # Adding Person object to the list of members
+            member_name = str(
+                input("     {} already in the team. Enter the name of team member {}: ".format(member_name, i + 1)))
+        team_members.append(Person(member_name))  # Adding Person object to the list of members
     print('\n')
     any_key = input('Press any key to return to the main menu: ')
     if type(any_key) == str:
@@ -65,13 +66,13 @@ def enter_votes():  # This function calculates the share of each member in a giv
     num_of_members = len(chosen_project.members)
     print('There are {} team members.'.format(num_of_members))
     # The process of assigning votes starts here
-    points_given = []   # A list of the sum of votes given by each member
-    points_assigned_correctly = False   # All values need to be equal to 100, ergo False
+    points_given = []  # A list of the sum of votes given by each member
+    points_assigned_correctly = False  # All values need to be equal to 100, ergo False
     while not points_assigned_correctly:
         # Iterating through all members in a project
         for member in chosen_project.members:
             print("\nEnter {}'s votes, points must add up to 100:\n".format(member.name))
-            points_left = 100   # The number of disposable votes for each member
+            points_left = 100  # The number of disposable votes for each member
             # Iterating through all partners of a member
             how_many_partners_left = len(chosen_project.members) - 2
             # This will be used to ensure that no partner is left with zero points (check WHILE NOT below)
@@ -82,12 +83,13 @@ def enter_votes():  # This function calculates the share of each member in a giv
                     # Checking if the votes are non-negative
                     # And their sum is not greater than disposable votes
                     while not (points.isdigit() and 0 < int(points) <= points_left - HMPL):
-                        points = input("Incorrect input. Enter {}'s points for {}:".format(member.name, other_member.name))
+                        points = input(
+                            "Incorrect input. Enter {}'s points for {}:".format(member.name, other_member.name))
                     other_member.votes.append(int(points))  # Adding votes to a member
                     points_left -= int(points)  # Decreasing the number of disposable votes left
                     HMPL -= 1
-            points_given.append(100 - points_left) # Adding the number of points given by each member (should be 100)
-        points_assigned_correctly = True    # Assuming that all values in points_given are 100
+            points_given.append(100 - points_left)  # Adding the number of points given by each member (should be 100)
+        points_assigned_correctly = True  # Assuming that all values in points_given are 100
         # Checking if each member's votes add up to 100.
         # If they don't, any changes are cancelled. Iterate through members again (while loop)
         for x in points_given:
@@ -104,12 +106,11 @@ def enter_votes():  # This function calculates the share of each member in a giv
         denominator = 1
         for vote in member.votes:
             denominator += (100 - vote) / vote
-        member.share = round(1 / denominator, 2)    # Rounding member's share to 2 decimal places
+        member.share = round(1 / denominator, 2)  # Rounding member's share to 2 decimal places
     # Masz tutaj wyniki gdybyś chciał sprawdzić jak to wszystko działa. Enjoy ^^
     for member in chosen_project.members:
         print('{} - {}'.format(member.name, member.share))
     return True
-
 
 
 # programme goes here
@@ -119,9 +120,9 @@ projects = []
 while option != 'Q':
     option = main_menu()
     if option == 'A':
-        about() # Displays information about the programme
+        about()  # Displays information about the programme
     elif option == create_project:
-        projects.append(create_new_project())   # Adds a new project object to the list
+        projects.append(create_new_project())  # Adds a new project object to the list
     elif option == 'V':
         enter_votes()
 
