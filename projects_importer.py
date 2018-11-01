99ecee444f65fac4bf280ed27f46d84b8970364d
Maciej Kaznowski <mmkaznowski@gmail.com>
2018-11-01 22:49:43 +0000

    Add basic error handling when importing projects

--- projects_importer.py
+++ projects_importer.py
@@ -16,8 +16,10 @@ class ProjectsImporter:
             print("Imported projects: ")
             for project in projects:
                 print(project)
-
-        except MalformedCsvError:
+            print("\n")
+        except MalformedCsvError as error:
+            print("Error importing projects: ")
+            print(error.exception)
             projects = []
 
         self.projects_repo.put(projects)
@@ -34,34 +36,39 @@ class ProjectsImporter:
 
     @staticmethod
     def __from_csv_line(row):
-        project_name = row[0]
-        member_count = int(row[1])
-        members = []
-        for member_index in range(member_count):
-            member_name = row[2 + member_index]
-            member = Person(member_name)
-            members.append(member)
-        for i in range(member_count):
-            from_index = 2 + member_count + i * (1 + (member_count - 1) * 2)
-            to_index = from_index + (1 + (member_count - 1) * 2)
-            vote_section = row[from_index:to_index]
-            voter_name = vote_section[0]
+        try:
+            project_name = row[0]
+            member_count = int(row[1])
+            members = []
+            for member_index in range(member_count):
+                member_name = row[2 + member_index]
+                member = Person(member_name)
+                members.append(member)
+            for i in range(member_count):
+                from_index = 2 + member_count + i * (1 + (member_count - 1) * 2)
+                to_index = from_index + (1 + (member_count - 1) * 2)
+                vote_section = row[from_index:to_index]
+                voter_name = vote_section[0]
 
-            for member in members:
-                if member.name == voter_name:
-                    for target_member_name_index in range(0, (member_count - 1) * 2, 2):
-                        target_member_name = vote_section[1 + target_member_name_index]
-                        target_member_vote = vote_section[1 + target_member_name_index + 1]
+                for member in members:
+                    if member.name == voter_name:
+                        for target_member_name_index in range(0, (member_count - 1) * 2, 2):
+                            target_member_name = vote_section[1 + target_member_name_index]
+                            target_member_vote = vote_section[1 + target_member_name_index + 1]
 
-                        for target_member in members:
-                            if target_member.name == target_member_name:
-                                member.votes[target_member] = int(target_member_vote)
-        project = Project(project_name, members)
-        project.calculate_shares()
-        return project
+                            for target_member in members:
+                                if target_member.name == target_member_name:
+                                    member.votes[target_member] = int(target_member_vote)
+            project = Project(project_name, members)
+            project.calculate_shares()
+            return project
+        except Exception as exception:
+            raise MalformedCsvError(row, exception)
 
 
 class MalformedCsvError(IOError):
 
-    def __init__(self, *args: object, **kwargs: object) -> None:
+    def __init__(self, row, exception, *args: object, **kwargs: object) -> None:
         super().__init__(*args, **kwargs)
+        self.row = row
+        self.exception = exception
