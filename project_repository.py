45a52439b1a154f1fe2ec31392cad6b438b918ee
Maciej Kaznowski <mmkaznowski@gmail.com>
2018-11-01 17:20:51 +0000

    Use a dict in ProjectRepository and test the class

--- project_repository.py
+++ project_repository.py
@@ -1,24 +1,29 @@
+from project import Project
+
+
 class ProjectRepository:
 
     def __init__(self) -> None:
         super().__init__()
-        self.__projects = []
+        self.__projects = {}
 
-    def put(self, projects):
-        if hasattr(projects, "__iter__"):
-            self.__projects.extend(projects)
+    def put(self, what):
+        if hasattr(what, "__iter__"):
+            for project in what:
+                self.put(project)
+        elif type(what) is Project:
+            self.__projects[what.name] = what
         else:
-            self.__projects.append(projects)
+            raise ValueError("Incorrect type %s" % type(what))
 
     def delete(self, project):
-        self.__projects.remove(project)
+        self.__projects.pop(project.name)
 
     def delete_all(self):
         self.__projects.clear()
 
     def get_all(self):
-        # TODO return a new projects list instead of existing, maybe via copy.deep_copy_list()?
-        return self.__projects
+        return set(self.__projects.values())
 
     def find_by_name(self, name):
         """
@@ -27,5 +32,4 @@ class ProjectRepository:
         :param name: The name of the project
         :return: The first project to be found with the given name, or None if there are none that have such a name
         """
-        projects_for_name = (project for project in self.__projects if project.name == name)
-        return next(projects_for_name, None)
+        return self.__projects.get(name)
---
+++ project_repository_test.py
@@ -0,0 +1,101 @@
+from unittest import TestCase
+
+from project import Person, Project
+from project_repository import ProjectRepository
+
+
+def create_project(project_name, member_names):
+    project_members = []
+    for name in member_names:
+        project_members.append(Person(name))
+
+    return Project(project_name, project_members)
+
+
+repo = ProjectRepository()
+
+project_1 = create_project("project_1", ["a", "b", "c"])
+project_2 = create_project("project_2", ["d", "e", "f"])
+
+
+class TestProjectRepository(TestCase):
+
+    def tearDown(self):
+        super().tearDown()
+        repo.delete_all()
+
+    def test_put(self):
+        self.assert_repo_eq([])
+
+        repo.put(project_1)
+        self.assert_repo_eq([project_1])
+
+    def test_put_duplicates(self):
+        self.assert_repo_eq([])
+
+        repo.put(project_1)
+        repo.put(project_1)
+        self.assert_repo_eq([project_1])
+
+    def test_put_list(self):
+        self.assert_repo_eq([])
+
+        repo.put([project_1, project_2])
+        self.assert_repo_eq([project_1, project_2])
+
+    def test_put_list_with_duplicates(self):
+        self.assert_repo_eq([])
+
+        repo.put([project_1, project_2, project_2])
+        self.assert_repo_eq([project_1, project_2])
+
+    def test_put_same_name(self):
+        repo.put(project_1)
+        self.assert_repo_eq([project_1])
+
+        # Check that if we attempt to put a project with the same name then it replaces the project with that name
+        project_with_same_name = create_project(project_1.name, ["d", "e", "f"])
+        repo.put(project_with_same_name)
+        self.assert_repo_eq([project_with_same_name])
+
+    def test_delete(self):
+        self.assert_repo_eq([])
+
+        repo.put(project_1)
+        self.assert_repo_eq([project_1])
+
+        repo.delete(project_1)
+        self.assert_repo_eq([])
+
+    def test_delete_all(self):
+        self.assert_repo_eq([])
+
+        repo.put(project_1)
+        repo.put(project_2)
+        self.assert_repo_eq([project_1, project_2])
+
+        repo.delete_all()
+        self.assert_repo_eq([])
+
+    def test_get_all(self):
+        self.assert_repo_eq([])
+
+        repo.put(project_1)
+        self.assert_repo_eq([project_1])
+
+        repo.put(project_2)
+        self.assert_repo_eq([project_1, project_2])
+
+    def test_find_by_name(self):
+        self.assert_repo_eq([])
+
+        self.assertIsNone(repo.find_by_name(project_1.name))
+
+        repo.put(project_1)
+        self.assertEqual(project_1, repo.find_by_name(project_1.name))
+
+        repo.put(project_2)
+        self.assertEqual(project_2, repo.find_by_name(project_2.name))
+
+    def assert_repo_eq(self, values):
+        self.assertEqual(set(values), repo.get_all())
