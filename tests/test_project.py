f99092c7b1139103d210121d32519e1da590a277
Maciej Kaznowski <mmkaznowski@gmail.com>
2018-11-14 10:13:38 +0000

    Split the equal tests in test_project.py

--- tests/test_project.py
+++ tests/test_project.py
@@ -28,31 +28,36 @@ class TestProject(TestCase):
         with self.assertRaises(ValueError):
             test_helper.create_project("not enough members", ["a", "b"])
 
-    def test_equals(self):
-        # Has exactly the same arguments passed to it
-        project_copy = test_helper.create_project(PROJECT_NAME, PROJECT_MEMBER_NAMES)
-        self.assertEqual(project, project_copy)
+    def test_eq_members_in_different_order(self):
+        # Same project but with members order reversed
+        members = list(reversed(PROJECT_MEMBER_NAMES))
+        project_reversed_members = test_helper.create_project(PROJECT_NAME, members)
+        self.assertEqual(project, project_reversed_members)
 
-        # Different project name and all members have the same name
-        project_with_diff_name = test_helper.create_project(PROJECT_NAME + ":1", PROJECT_MEMBER_NAMES)
-        self.assertNotEqual(project, project_with_diff_name)
-
-        # Same project name but all members have a different name
-        diff_member_names = list(map(lambda name: name + "_diff", PROJECT_MEMBER_NAMES))
-        project_with_diff_member_names = test_helper.create_project(PROJECT_NAME, diff_member_names)
-        self.assertNotEqual(project, project_with_diff_member_names)
-
-        # Same project name and has the same members + additional members
-        project_more_members = test_helper.create_project(PROJECT_NAME, PROJECT_MEMBER_NAMES + ["e"])
-        self.assertNotEqual(project, project_more_members)
-
+    def test_eq_fewer_members(self):
         # Same project name but fewer members
         fewer_members = PROJECT_MEMBER_NAMES.copy()
         fewer_members.remove("d")
         project_fewer_members = test_helper.create_project(PROJECT_NAME, fewer_members)
         self.assertNotEqual(project, project_fewer_members)
 
-        # Same project but with members order reversed
-        members = list(reversed(PROJECT_MEMBER_NAMES))
-        project_reversed_members = test_helper.create_project(PROJECT_NAME, members)
-        self.assertEqual(project, project_reversed_members)
+    def test_eq_more_members(self):
+        # Same project name and has the same members + additional members
+        project_more_members = test_helper.create_project(PROJECT_NAME, PROJECT_MEMBER_NAMES + ["e"])
+        self.assertNotEqual(project, project_more_members)
+
+    def test_eq_diff_member_names(self):
+        # Same project name but all members have a different name
+        diff_member_names = list(map(lambda name: name + "_diff", PROJECT_MEMBER_NAMES))
+        project_with_diff_member_names = test_helper.create_project(PROJECT_NAME, diff_member_names)
+        self.assertNotEqual(project, project_with_diff_member_names)
+
+    def test_eq_diff_names(self):
+        # Different project name and all members have the same name
+        project_with_diff_name = test_helper.create_project(PROJECT_NAME + ":1", PROJECT_MEMBER_NAMES)
+        self.assertNotEqual(project, project_with_diff_name)
+
+    def test_eq_same_args(self):
+        # Has exactly the same arguments passed to it
+        project_copy = test_helper.create_project(PROJECT_NAME, PROJECT_MEMBER_NAMES)
+        self.assertEqual(project, project_copy)
