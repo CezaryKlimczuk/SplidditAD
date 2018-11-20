aef34ffa29341752a9e33d3f829dba7a5228fd72
Maciej Kaznowski <mmkaznowski@gmail.com>
2018-11-20 21:05:27 +0000

    Document projects_exporter.py

--- csv/projects_exporter.py
+++ csv/projects_exporter.py
@@ -3,6 +3,12 @@ import os
 
 
 class ProjectsExporter:
+    """
+    This class is responsible for exporting all the projects which are in the ProjectsRepo to the provided file.
+    Ideally, the ProjectsRepo would be in control of persisting its state to io, however project requirements dictate
+    that projects should be exported/imported at the start/exit of the program, rather than upon creating a new Project
+    If the provided file does not exist, it creates it. If the files does exist, it is overwritten.
+    """
 
     def __init__(self, file_name, projects_repo) -> None:
         super().__init__()
@@ -10,6 +16,10 @@ class ProjectsExporter:
         self.projects_repo = projects_repo
 
     def export_projects(self):
+        """
+        Exports all the projects in the repo to the provided csv file. Overwrites any current such file.
+        """
+        print("Exporting %s projects to %s" % (len(self.projects_repo.get_all()), self.file_name))
         lines = []
         for project in self.projects_repo.get_all():
             try:
@@ -23,11 +33,19 @@ class ProjectsExporter:
 
     @staticmethod
     def __to_csv_line(project):
+        """
+        Create a list of elements to export the given project to csv
+
+        :param project: The project which we want to create a csv line from.
+        :return: a list of strings which can be exported as csv by separating each element with a delimiter.
+        :raises ValueError when the give project cannot be exported because the user hasn't entered all the votes.
+        """
         for member in project.members:
             if member.get_remaining_votes() != 0:
                 # Cannot export projects where the user hasn't filled the votes
                 raise ValueError("Cannot export project \"%s\" because the votes haven't been entered" % project.name)
 
+        # a list of values which will be stored in each line of the csv
         elements = [project.name, project.get_member_count()]
 
         # Appending member names
@@ -44,6 +62,13 @@ class ProjectsExporter:
         return elements
 
     def __get_file(self):
+        """
+        Returns the file to write export the projects to.
+        If the file exists, it will be overwritten.
+        If it doesn't exist, it will be created
+
+        :return: The file to be used to export the projects
+        """
         if not os.path.exists(self.file_name):
             # when calling open() the "+" mode will create the file if it does not exist
             print("File \"%s\" does not exist to export projects. Creating file." % self.file_name)
