a67401241cc1f8d56bd6d622ad5e8445ac0c5e56
Maciej Kaznowski <mmkaznowski@gmail.com>
2018-11-28 10:58:15 +0000

    Change project_retriever.py repo visibility

--- project/project_retriever.py
+++ project/project_retriever.py
@@ -2,7 +2,7 @@ class ProjectRetriever:
 
     def __init__(self, repo) -> None:
         super().__init__()
-        self.repo = repo
+        self.__repo = repo
 
     def get_project_from_user(self):
         """
@@ -11,10 +11,10 @@ class ProjectRetriever:
         :return: a Project
         """
         name = input('Enter the project name: ')
-        project = self.repo.find_by_name(name)
+        project = self.__repo.find_by_name(name)
 
         while project is None:
             name = input('Incorrect project name, please enter the project name:')
-            project = self.repo.find_by_name(name)
+            project = self.__repo.find_by_name(name)
 
         return project
